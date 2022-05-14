df15ea678e1e8dc5a545bd122ad4d291daa4e6ea
Rahul Chauhan <optimist49chauhan@gmail.com>
2022-05-14 23:20:04 +0530

    add feature in maze

---
+++ maze.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number to check if it is prime or not ";
+    cin>>n;
+    return 0;
+}
